32a33bef89c741482cfd32550d4431ec6356a783
nguyenlyy2911 <nguyenlyy2911@gmail.com>
2025-06-13 18:22:59 +0700

    Add files via upload

---
+++ web/js/control.js
@@ -0,0 +1,16 @@
+// JavaScript Document
+	var typed = new Typed('.element', {
+strings: ["Web Designer.", "Code Developer.","Apps Designer.","Film Maker."],
+typeSpeed:20,
+backSpeed:20,
+loop:true,
+});
+
+
+$('.timeline-panel').click(function() {
+    $('.timeline-body', this).toggle(); // p00f
+});
+
+$('.timeline-panel').click(function() {
+    $('.timeline-body', this).toggle(); // p00f
+});
